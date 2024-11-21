d37e44c48c1567fc9c89c0a0b375995a3dc9ffd2
Miro <miro.grare@student.ap.be>
2024-11-21 13:40:16 +0100

    Implement stap 2

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 import requests
 import click
+from bs4 import BeautifulSoup
+import re
 
 
 @click.command()
 @click.argument("url")
 def gethtml(url):
     r = requests.get(url)
-    print(r.text)
+
+    soup = BeautifulSoup(r.text, "html.parser")
+    text = soup.get_text()
+
+    words = re.findall(r"\b\w+\b", text)
+
+    for word in words:
+        print(word)
 
 
 if __name__ == "__main__":
